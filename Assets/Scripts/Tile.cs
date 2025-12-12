4dca2adacf3b672771ae84de6837b505e2aae035
RaymondWoon <58698435+RaymondWoon@users.noreply.github.com>
2025-12-11 22:28:40 -0500

    Add Dungeon class
    Add tile and dungeon classes

---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+
+public class Tile
+{
+
+    public enum TileType
+    {
+        Wall,
+        Room,
+        Corridor,
+        Door
+    }
+
+    private TileType tType;
+    private bool isVisible;
+
+    /// <summary>
+    /// The type of tile
+    /// </summary>
+    public TileType Type
+    {
+        get { return tType; }
+        set { tType = value; }
+    }
+
+    /// <summary>
+    /// Set visibility of tile.
+    /// Used after dungeon generation to hide unused areas.
+    /// </summary>
+    public bool IsVisible
+    {
+        get { return isVisible; }
+        set { isVisible = value; }
+    }
+
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+    public Tile()
+    {
+        tType = TileType.Wall;
+        isVisible = true;
+    }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="_tType"></param>
+    /// <param name="_isVisible"></param>
+    public Tile(TileType _tType, bool _isVisible)
+    {
+        tType = _tType;
+        isVisible = _isVisible;
+    }
+
+    public void PrintTile(Tile tile)
+    {
+        Console.WriteLine("Type: " + tile.Type + ", isVisible: " + (tile.isVisible ? "1" : "0"));
+    }
+
+    public override string ToString()
+    {
+        return ("Type: " + tType + ", isVisible: " + (isVisible ? "1" : "0"));
+    }
+}
